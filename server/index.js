4345819d63bf0fd10850273419c3161605126db0
Toomas Tahves <toomastahves@hotmail.com>
2016-01-23 19:28:20 +0200

    moved dependencies around

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 // Support for more ES6 features in Node.
 require('babel-core/register');
-if(process.env.NODE_ENV === 'development') {
-  require('./server.dev.js');
-}
+
 if(process.env.NODE_ENV === 'production') {
   require('./server.prod.js');
+} else {
+  require('./server.dev.js');
 }
