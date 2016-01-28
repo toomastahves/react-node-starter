8ca6911f6919ecddb69b6b8a1c89cc82688fc74e
Toomas Tahves <toomastahves@hotmail.com>
2016-01-28 21:39:10 +0200

    minor refactoring

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import style from './style.css';
-export class App extends Component {
-  render() {
-    return (
-      <div className={style.center}>
-        {'hello world'}
-      </div>
-    );
-  }
-}
+
+export const App = () => {
+  return (
+    <div className={style.center}>
+      {'hello world'}
+    </div>
+  );
+};
+
 export default App;
