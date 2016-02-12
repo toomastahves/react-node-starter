92a9257caedde4b8ffe825f7a75b7148df9dcb00
Toomas Tahves <toomastahves@hotmail.com>
2016-02-12 23:33:41 +0200

    added redux dev tools and updated dependencies

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import { routeReducer } from 'react-router-redux';
 import { helloReducer } from './hello';
 
 const reducers = combineReducers({
-  routeReducer,
   helloReducer
 });
 
--- client/store/index.js
+++ client/store/index.js
@@ -1,15 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from '../reducers/';
 import thunkMiddleware from 'redux-thunk';
-import { syncHistory } from 'react-router-redux';
-import { useRouterHistory } from 'react-router';
-import { createHashHistory } from 'history';
 
-const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
-
-const reduxRouterMiddleware = syncHistory(appHistory);
-
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware, reduxRouterMiddleware)(createStore);
+const createStoreWithMiddleware = compose(
+  applyMiddleware(thunkMiddleware),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
 
 const store = createStoreWithMiddleware(reducers);
 
