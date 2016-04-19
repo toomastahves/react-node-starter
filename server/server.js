59eb7035a1aa7fef24ec4d7cb506e268fd473290
Toomas Tahves <toomastahves@users.noreply.github.com>
2016-04-19 19:56:39 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import app from './koa/app';
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Express started at ${port}`);
+  console.log(`Koa started at ${port}`);
 });
 
 if(process.env.NODE_ENV !== 'production') {
