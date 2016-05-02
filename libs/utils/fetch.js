124ac2ee271a1a7422dbb89aa10594d68862f455
Toomas Tahves <toomastahves@hotmail.com>
2016-05-02 14:06:05 +0300

    updated fetch method

--- libs/utils/fetch.js
+++ libs/utils/fetch.js
@@ -1,31 +1,21 @@
 import { SERVER_URI } from '../../client/constants/';
 
 export const fetch = ({ path, type, data }) => {
-  const url = `${API_URI}${path}`;
+  const url = `${SERVER_URI}${path}`;
   console.log(`${type} ${url}`);
   return new Promise((resolve, reject) => {
-    let req = new XMLHttpRequest();
+    const req = new XMLHttpRequest();
+    req.open(type, url, true);
 
-    // CORS support
-    if ('withCredentials' in req) {
-      req.open(type, url, true);
-    } else if (typeof XDomainRequest != 'undefined') {
-      req = new XDomainRequest();
-      req.open(type, url);
-    } else {
-      req = null;
-    }
-
-    req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
     req.onload = function() {
       if(req.status === 200) {
         resolve(JSON.parse(req.response));
       } else {
-        reject(JSON.parse(req.response));
+        reject(req.response);
       }
     };
     req.onerror = function() {
-      reject(JSON.parse({ error: { message: 'Network error' } }));
+      reject('Network error');
     };
     req.send(data);
   });
